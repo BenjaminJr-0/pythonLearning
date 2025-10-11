eadfcd4b2225d428135d38ee3fb96844f5d64a3a
Benjamin Mendosa <benjaminmendosa1202@gmail.com>
2025-10-10 22:24:07 -0700

    adding a test file

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import csv
+from matplotlib import pyplot as plt
+import copy
+import math
+
+x_train = np.array([60,40,20,4])
+y_train = np.array([20, 50, 80, 90])
+plt.plot(x_train, y_train)
+#plt.plot([x_train, y_train])
+plt.show()
+
+
+#check out np.array([[2, 5]
+#                  # [3, 9]])
+
+#look into artificial genreal intelligence (AGI) and Neural Networking Implementations
